edcc583a33015231131cb6a75bbbea94c1df0956
Derek Kastner <dkastner@gmail.com>
2011-12-22 15:46:12 -0500

    Initial API with passing integration tests

--- spec/concur_connect_spec.rb
+++ spec/concur_connect_spec.rb
@@ -5,20 +5,26 @@ describe ConcurConnect do
     concur = ConcurConnect.session '3dcPjZyTeQziAndLnUALTI', 'ZipGipKjsd4ypFx2qcJ5sCuBY9FYbJlE', 't0026855k9r8'
     concur.should be_a(ConcurConnect::Session)
     user = nil
+    expense_reports = nil
 
-    VCR.use_cassette('user', :record => :once) do
+    VCR.use_cassette('session expense reports', :record => :once) do
+      expense_reports = concur.expense_reports Date.new(2011, 9, 1)
+      expense_reports.should_not be_empty
+    end
+
+    VCR.use_cassette('session expenses', :record => :once) do
+      expenses = expense_reports.first.expenses
+      expenses.should_not be_empty
+    end
+
+    VCR.use_cassette('session user', :record => :once) do
       user = concur.user('derek.kastner@brighterplanet.com')
       user.should be_a(ConcurConnect::User)
     end
 
-    VCR.use_cassette('user expense reports', :record => :once) do
-      reports = user.expense_reports
-      reports.should_not be_empty
-    end
-
-    #trips = concur.trips.find 'example.com', 'bob@example.com'
-    #trips.each  { |trip| trip.should be_a(ConcurConnect::Trip) }
-    #trips = concur.trips.find 'example.com', 'bob@example.com', :start_date => Date.now
-    #trips.each  { |trip| trip.should be_a(ConcurConnect::Trip) }
+    #VCR.use_cassette('session user expense reports', :record => :once) do
+      #reports = user.expense_reports
+      #reports.should_not be_empty
+    #end
   end
 end
