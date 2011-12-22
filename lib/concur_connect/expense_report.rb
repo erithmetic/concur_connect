200acd534d5a3c5f3dd9443216a11b4d6787db1c
Derek Kastner <dkastner@gmail.com>
2011-12-22 14:58:44 -0500

    Add ExpenseReport#expenses

--- lib/concur_connect/expense_report.rb
+++ lib/concur_connect/expense_report.rb
@@ -1,5 +1,17 @@
+require 'concur_connect/expense_finder'
+
 module ConcurConnect
   class ExpenseReport
-    attr_accessor :id, :name, :date
+    attr_accessor :id, :name, :date,
+      :expenses,
+      :session
+
+    def expense_finder
+      @expense_finder ||= ExpenseFinder.new session
+    end
+
+    def expenses
+      @expenses ||= expense_finder.find id
+    end
   end
 end
--- lib/concur_connect/expense_report_finder.rb
+++ lib/concur_connect/expense_report_finder.rb
@@ -33,6 +33,7 @@ module ConcurConnect
           expense_report.id = datum['ReportId']
           expense_report.name = datum['ReportName']
           expense_report.date = datum['ReportDate']
+          expense_report.session = session
           list << expense_report
         end
       end
