fb34fd2d2d1bd69dbcf21d835cd77fe16137443a
Derek Kastner <dkastner@gmail.com>
2012-01-13 10:12:07 -0500

    Forward all the methods from ExpenseDetail to Expense

--- lib/concur_connect/expense.rb
+++ lib/concur_connect/expense.rb
@@ -1,16 +1,18 @@
 require 'concur_connect/data_item'
 require 'concur_connect/expense_detail_finder'
+require 'forwardable'
 
 module ConcurConnect
   class Expense < DataItem
+    extend Forwardable
+
     attr_accessor :type, :amount, :vendor, :transaction_date, :session, :details_url
+    def_delegators :details, *(ExpenseDetail::ATTRIBUTES + ExpenseDetail::ALIASES.keys)
 
     def details
       return @details unless @details.nil?
       finder = ExpenseDetailFinder.new session
       @details ||= finder.find(details_url)
     end
-
-    def transaction_date; details.transaction_date; end
   end
 end
--- lib/concur_connect/expense_detail.rb
+++ lib/concur_connect/expense_detail.rb
@@ -1,23 +1,28 @@
 module ConcurConnect
   class ExpenseDetail < DataItem
-    attr_accessor :approved_amount, :business_purpose, :e_receipt_id,
-        :exchange_rate, :key, :name, :allocated, :attended, :commented,
-        :exceptional, :value_added_taxed, :image_required, :charged_by_credit_card,
-        :itemized, :personal, :personal_card_charge, :last_modified, :location,
-        :payment_type, :posted_amount, :receipt_image_id, :receipt_required,
-        :report_entry_id, :report_id, :transaction_amount, :transaction_currency,
-        :transaction_date, :vendor_description
+    ATTRIBUTES = [:approved_amount, :business_purpose, :e_receipt_id,
+      :exchange_rate, :key, :name, :allocated, :attended, :commented,
+      :exceptional, :value_added_taxed, :image_required, :charged_by_credit_card,
+      :itemized, :personal, :personal_card_charge, :last_modified, :location,
+      :payment_type, :posted_amount, :receipt_image_id, :receipt_required,
+      :report_entry_id, :report_id, :transaction_amount, :transaction_currency,
+      :transaction_date, :vendor_description]
+    ALIASES = {:allocated? => :allocated,
+               :attended? => :attended,
+               :commented? => :commented,
+               :exceptional? => :exceptional,
+               :value_added_taxed? => :value_added_taxed,
+               :image_required? => :image_required,
+               :charged_by_credit_card? => :charged_by_credit_card,
+               :itemized? => :itemized,
+               :personal? => :personal,
+               :personal_card_charge? => :personal_card_charge,
+               :receipt_required? => :receipt_required }
 
-    alias :allocated? :allocated
-    alias :attended? :attended
-    alias :commented? :commented
-    alias :exceptional? :exceptional
-    alias :value_added_taxed? :value_added_taxed
-    alias :image_required? :image_required
-    alias :charged_by_credit_card? :charged_by_credit_card
-    alias :itemized? :itemized
-    alias :personal? :personal
-    alias :personal_card_charge? :personal_card_charge
-    alias :receipt_required? :receipt_required
+    attr_accessor *ATTRIBUTES
+
+    ALIASES.each do |question, attribute|
+      send :alias_method, question, attribute
+    end
   end
 end
